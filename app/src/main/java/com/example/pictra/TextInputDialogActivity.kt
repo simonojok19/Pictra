4f5b841e25e1030a77dca84a9d5e218209ad79d4
simonojok19 <simonojok19@gmail.com>
2021-08-21 13:15:06 +0300

    adding name picker activity

---
+++ app/src/main/java/com/example/pictra/TextInputDialogActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pictra
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.pictra.R
+
+class TextInputDialogActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_input_dialog)
+    }
+}
