4a5ab837e084e7eed0ece04e9e48e2b08110233b
simonojok19 <simonojok19@gmail.com>
2021-08-21 13:34:15 +0300

    added the text picker for the activity

--- app/src/main/java/com/example/pictra/TextInputActivity.kt
+++ app/src/main/java/com/example/pictra/TextInputActivity.kt
@@ -1,12 +1,31 @@
 package com.example.pictra
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class TextInputActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_input)
+
+        val addButton: Button = findViewById(R.id.add_button)
+        addButton.setOnClickListener {
+            val input: TextView = findViewById(R.id.text_input)
+            val text = input.text.toString()
+            if (text.isNotEmpty()) {
+                val intent = Intent()
+                intent.putExtra(TEXT_ENTERED, text)
+                setResult(RESULT_OK, intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Please enter some value", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 
     companion object {
