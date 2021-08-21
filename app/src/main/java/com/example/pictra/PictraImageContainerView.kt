eaf03593e158c3c82a97565fcca7cdbd59686304
simonojok19 <simonojok19@gmail.com>
2021-08-21 10:37:15 +0300

    saving file now to galary

--- app/src/main/java/com/example/pictra/PictraImageContainerView.kt
+++ app/src/main/java/com/example/pictra/PictraImageContainerView.kt
@@ -1,8 +1,12 @@
 package com.example.pictra
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import android.util.AttributeSet
 import kotlin.jvm.JvmOverloads
 import android.widget.RelativeLayout
@@ -12,9 +16,14 @@ import android.view.LayoutInflater
 import androidx.core.content.ContentProviderCompat.requireContext
 import java.io.File
 import android.os.Environment
+import android.provider.MediaStore
 import androidx.core.graphics.drawable.toBitmap
 import java.io.ByteArrayOutputStream
 import java.io.FileOutputStream
+import java.io.OutputStream
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class PictraImageContainerView @JvmOverloads constructor(
@@ -64,7 +73,7 @@ class PictraImageContainerView @JvmOverloads constructor(
     fun saveImageToFile(file: File) {
         pictraImageView.buildDrawingCache()
         val bitmap = pictraImageView.drawingCache
-        saveBitmap(bitmap, file)
+        saveMediaToStorage(bitmap)
     }
 
     fun resetView() {
@@ -94,13 +103,46 @@ class PictraImageContainerView @JvmOverloads constructor(
         initView()
     }
 
-    fun saveBitmap(bmp: Bitmap, file: File): File {
-        val bytes = ByteArrayOutputStream()
-        bmp.compress(Bitmap.CompressFormat.JPEG, 60, bytes)
-        file.createNewFile()
-        val fo = FileOutputStream(file)
-        fo.write(bytes.toByteArray())
-        fo.close()
-        return file
+    fun saveMediaToStorage(bitmap: Bitmap) {
+        //Generating a file name
+        val filename = "pictra_${System.currentTimeMillis()}.jpg"
+
+        //Output stream
+        var fos: OutputStream? = null
+
+        //For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            context?.contentResolver?.also { resolver ->
+
+                //Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+        }
     }
 }
